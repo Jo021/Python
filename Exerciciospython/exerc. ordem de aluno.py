f8efb316cd1198521a1022a9d0fc7902f66fba9d
NovaesJR <99695427+Jo021@users.noreply.github.com>
2022-02-17 19:34:55 -0300

    Update exerc. ordem de aluno.py

--- Exerciciospython/exerc. ordem de aluno.py
+++ Exerciciospython/exerc. ordem de aluno.py
@@ -1,6 +1,6 @@
 from random import shuffle
 
-print(" Do mais gay ao menos gay")
+print(" Sorteio dos alunos")
 pessoa1 = str(input(" Nome: "))
 pessoa2 = str(input(" Nome: "))
 pessoa3 = str(input(" Nome: "))
@@ -8,6 +8,6 @@ pessoa4 = str(input(" Nome: "))
 pessoa5 = str(input(" Nome: "))
 sorteio = [pessoa1, pessoa2, pessoa3, pessoa4, pessoa5]
 shuffle(sorteio)
-print(" A verdade é essa: ")
+print(" O escolhido foi: ")
 print("========RESULTADO=======")
 print(f" {sorteio}")
