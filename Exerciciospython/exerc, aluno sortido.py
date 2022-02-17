33582eeb27766aaa6054f37d8f898fdd164a5f55
NovaesJR <99695427+Jo021@users.noreply.github.com>
2022-02-17 19:34:04 -0300

    Update exerc, aluno sortido.py

--- Exerciciospython/exerc, aluno sortido.py
+++ Exerciciospython/exerc, aluno sortido.py
@@ -1,5 +1,5 @@
 import random
-print(" Qual deles é o mais gay?")
+print(" Qual deles é o melhor?")
 alun1 = str(input(" Primeiro(a): "))
 alun2 = str(input(" Segundo(a): "))
 alun3 = str(input(" Terceiro(a): "))
@@ -7,4 +7,4 @@ alun4 = str(input(" Quarto(a): "))
 alun5 = str(input(" Quinto(a): "))
 lista = [alun1, alun2, alun3, alun4, alun5]
 escolhido = random.choice(lista)
-print(f" {escolhido} é o mais gay!")
+print(f" {escolhido} é o melhor"!)
